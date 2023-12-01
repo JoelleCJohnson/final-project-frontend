a2f3a448fdd4c885535fca95dca72f960ca178fd
Joelle  Johnson <joellej221@gmail.com>
2023-12-01 14:56:18 -0500

    handle login- not yet functional

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,6 +1,3 @@
-import DisplayWishlist from "@/app/dashboard/DisplayWishList"
-import AddItem from "@/app/dashboard/AddItem"
-
 export default function DashboardLayout({ children }){
     return(
         <main >
--- app/login/Login.jsx
+++ app/login/Login.jsx
@@ -1,27 +1,66 @@
 'use client'
 import { Button, Checkbox, Label, TextInput } from 'flowbite-react';
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { UserContext } from "@/context/UserContext"
+import { useRouter } from 'next/navigation';
 
 export default function Login() {
 
     const { token, setToken, loggedin, setLoggedIn } = useContext(UserContext)
+    const [ error, setError ] = useState(null)
+
+    const route = useRouter()
+
+    const handleLogin = (token) => {
+        // if(token.message){
+        //     setError(token.message)
+        //     return
+        // }
+          setToken(token.token)
+          setError()
+          route.push('/dashboard')
+      }
+
+    const handleFormSubmit = (e) => {
+
+        e.preventDefault()
+
+        const formData = {
+            email: e.target.email.value,
+            password: e.target.password.value
+        }
+
+         fetch('https://holiday-wishlist-jj.ue.r.appspot.com/login', {
+            method: 'POST',
+            headers: {
+              'Content-type': 'application/json',
+            },
+            body: JSON.stringify(formData)
+         })
+            .then(res => res.json())
+            .then(handleLogin)
+            .catch(console.error)
+
+    }
 
     return (
-        <form className="flex max-w-md flex-col gap-4 m-4 bg-red-600 p-4 rounded-lg">
+        <form className="flex max-w-md flex-col gap-4 m-4 bg-red-600 p-4 rounded-lg" onSubmit={handleFormSubmit}>
             <h2 className='flex justify-center text-white'>Login:</h2>
+
             <div>
                 <div className="mb-2 block">
                     <Label htmlFor="email" value="Your email" />
                 </div>
                 <TextInput id="email" type="email" placeholder="name@email.com" required />
             </div>
+
             <div>
                 <div className="mb-2 block">
                     <Label htmlFor="password" value="Your password" />
                 </div>
                 <TextInput id="password" type="password" required />
             </div>
+
             <div className="flex items-center gap-2">
                 <Checkbox id="remember" />
                 <Label htmlFor="remember">Remember me</Label>
