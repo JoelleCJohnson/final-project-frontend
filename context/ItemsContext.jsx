b8a10b3b974b9d2e2a218d6e26e97505cc074a6e
Joelle  Johnson <joellej221@gmail.com>
2023-12-02 14:22:15 -0500

    removed useEffect

--- context/ItemsContext.jsx
+++ context/ItemsContext.jsx
@@ -1,20 +1,11 @@
 "use client"
-import { createContext, useEffect, useState } from "react"
+import { createContext, useState } from "react"
 
 export const ItemContext = createContext(null)
 
 export default function ItemsContext({ children }) {
 
     const [wishlist, setWishlist] = useState([])
-
-    // const listItems = () => {
-    //     fetch('https://holiday-wishlist-jj.ue.r.appspot.com/')
-    //         .then(res => res.json())
-    //         .then(setWishlist)
-    //         .catch(console.error)
-    // }
-
-    // useEffect(listItems, [])
 
     return (
         <ItemContext.Provider value={{ wishlist, setWishlist }}>
