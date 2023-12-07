ed192dca7c99821a698dd7d64a2d4ffa0d8d5e6e
Joelle  Johnson <joellej221@gmail.com>
2023-12-07 16:09:43 -0500

    styling

--- app/page.js
+++ app/page.js
@@ -7,9 +7,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='bg-white h-screen'>
-      <div className="absolute inset-0">
+      
       <Header />
-      </div>
+      
       <Login />
       {/* <Footer /> */}
     </main>
